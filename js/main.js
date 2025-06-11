6541986d79de70887a9f335e102fdbd768772e17
Тёмкаа. Тёмкаа. <k3nnel123@gmail.com>
2025-06-11 03:40:42 +0300

    finished: testimonial and brand sections

--- js/main.js
+++ js/main.js
@@ -4,3 +4,25 @@ const menuList = document.getElementById("menu-list");
 navTriggerBtn.addEventListener("click", () => {
   menuList.classList.toggle("nav-is-open");
 });
+
+// swiper
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  slidesPerView: 3,
+  spaceBetween: 20,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    960: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
